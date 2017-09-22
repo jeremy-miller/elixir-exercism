fe4a9e2300e020e6eb66711de927bcb79d85c835
Jeremy <jgmiller88@gmail.com>
2017-09-21 22:44:27 -0700

    rotational cipher working

--- lib/rotational_cipher.ex
+++ lib/rotational_cipher.ex
@@ -3,6 +3,15 @@ defmodule RotationalCipher do
   Solution for [Rotational Cipher](http://exercism.io/exercises/elixir/rotational-cipher/readme) exercise.
   """
 
+  import Enum, only: [at: 2, find_index: 2, join: 2, map: 2]
+  import Integer, only: [mod: 2]
+  import String, only: [codepoints: 1]
+
+  @lowercase_alphabet ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p",
+  "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+  @uppercase_alphabet ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P",
+  "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+
   @doc ~S"""
   Shifts all letters in the `plaintext` by the same amount and returns the shifted ciphertext.
   Letters are shifted for as many values as the value of `key`.
@@ -18,6 +27,22 @@ defmodule RotationalCipher do
   """
   @spec rotate(String.t, integer) :: String.t
   def rotate(plaintext, key) do
-    
+    plaintext
+    |> codepoints
+    |> map(fn(character) ->
+         cond do
+           character in @lowercase_alphabet -> lookup_character(character, @lowercase_alphabet, key)
+           character in @uppercase_alphabet -> lookup_character(character, @uppercase_alphabet, key)
+           true -> character
+         end
+       end)
+    |> join("")
+  end
+
+  defp lookup_character(character, alphabet, key) do
+    alphabet
+    |> find_index(fn(letter) -> letter === character end)
+    |> (&(mod(&1 + key, 26))).()
+    |> (&(at(alphabet, &1))).()
   end
 end
