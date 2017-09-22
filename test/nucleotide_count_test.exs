c4742e50c2278f139eadacdc1226f88ac7902855
Jeremy <jgmiller88@gmail.com>
2017-09-22 07:05:55 -0700

    add describe to test files

--- test/nucleotide_count_test.exs
+++ test/nucleotide_count_test.exs
@@ -4,47 +4,51 @@ defmodule NucleotideCountTest do
   import Map, only: [has_key?: 2, keys: 1]
   doctest NucleotideCount
 
-  test "NucleotideCount.count correctly counts valid nucleotides" do
-    ptest dna_string: string(chars: :upper, must_have: ["A", "T", "C", "G"]), nucleotide: string(min: 1, max: 1, chars: :upper, must_have: ["A", "T", "C", "G"]) do
-      assert NucleotideCount.count(dna_string, nucleotide) >= 0
+  describe "NucleotideCount.count/2" do
+    test "correctly counts valid nucleotides" do
+      ptest dna_string: string(chars: :upper, must_have: ["A", "T", "C", "G"]), nucleotide: string(min: 1, max: 1, chars: :upper, must_have: ["A", "T", "C", "G"]) do
+        assert NucleotideCount.count(dna_string, nucleotide) >= 0
+      end
+    end
+
+    test "correctly counts nucleotides with invalid input" do
+      ptest dna_string: string(chars: :upper), nucleotide: string(min: 1, max: 1, chars: :upper, must_have: ["A", "T", "C", "G"]) do
+        assert NucleotideCount.count(dna_string, nucleotide) >= 0
+      end
     end
   end
 
-  test "NucleotideCount.count correctly counts nucleotides with invalid input" do
-    ptest dna_string: string(chars: :upper), nucleotide: string(min: 1, max: 1, chars: :upper, must_have: ["A", "T", "C", "G"]) do
-      assert NucleotideCount.count(dna_string, nucleotide) >= 0
+  describe "NucleotideCount.histogram/1" do
+    test "NucleotideCount.histogram correctly counts valid nucleotides" do
+      ptest dna_string: string(chars: :upper, must_have: ["A", "T", "C", "G"]) do
+        histogram = NucleotideCount.histogram(dna_string)
+        histogram_keys_length = keys(histogram) |> length
+        assert histogram_keys_length === 4
+        assert has_key?(histogram, A)
+        assert has_key?(histogram, T)
+        assert has_key?(histogram, C)
+        assert has_key?(histogram, G)
+        assert histogram[A] >= 0
+        assert histogram[T] >= 0
+        assert histogram[C] >= 0
+        assert histogram[G] >= 0
+      end
     end
-  end
 
-  test "NucleotideCount.histogram correctly counts valid nucleotides" do
-    ptest dna_string: string(chars: :upper, must_have: ["A", "T", "C", "G"]) do
-      histogram = NucleotideCount.histogram(dna_string)
-      histogram_keys_length = keys(histogram) |> length
-      assert histogram_keys_length === 4
-      assert has_key?(histogram, A)
-      assert has_key?(histogram, T)
-      assert has_key?(histogram, C)
-      assert has_key?(histogram, G)
-      assert histogram[A] >= 0
-      assert histogram[T] >= 0
-      assert histogram[C] >= 0
-      assert histogram[G] >= 0
-    end
-  end
-
-  test "NucleotideCount.histogram correctly counts nucleotides with invalid input" do
-    ptest dna_string: string(chars: :upper) do
-      histogram = NucleotideCount.histogram(dna_string)
-      histogram_keys_length = keys(histogram) |> length
-      assert histogram_keys_length === 4
-      assert has_key?(histogram, A)
-      assert has_key?(histogram, T)
-      assert has_key?(histogram, C)
-      assert has_key?(histogram, G)
-      assert histogram[A] >= 0
-      assert histogram[T] >= 0
-      assert histogram[C] >= 0
-      assert histogram[G] >= 0
+    test "NucleotideCount.histogram correctly counts nucleotides with invalid input" do
+      ptest dna_string: string(chars: :upper) do
+        histogram = NucleotideCount.histogram(dna_string)
+        histogram_keys_length = keys(histogram) |> length
+        assert histogram_keys_length === 4
+        assert has_key?(histogram, A)
+        assert has_key?(histogram, T)
+        assert has_key?(histogram, C)
+        assert has_key?(histogram, G)
+        assert histogram[A] >= 0
+        assert histogram[T] >= 0
+        assert histogram[C] >= 0
+        assert histogram[G] >= 0
+      end
     end
   end
 end
--- test/secret_handshake_test.exs
+++ test/secret_handshake_test.exs
@@ -4,54 +4,56 @@ defmodule SecretHandshakeTest do
   import Enum, only: [all?: 2]
   doctest SecretHandshake
 
-  test "jump for 1000" do
-    assert SecretHandshake.commands(8) === ["jump"]
-  end
-
-  test "combine two actions" do
-    assert SecretHandshake.commands(3) === ["wink", "double blink"]
-  end
-
-  test "reverse two actions" do
-    assert SecretHandshake.commands(19) === ["double blink", "wink"]
-  end
-
-  test "reversing one action gives the same action" do
-    assert SecretHandshake.commands(24) === ["jump"]
-  end
-
-  test "reversing no actions still gives no actions" do
-    assert SecretHandshake.commands(16) === []
-  end
-
-  test "all possible actions" do
-    assert SecretHandshake.commands(15) === ["wink", "double blink", "close your eyes", "jump"]
-  end
-
-  test "reverse all possible actions" do
-    assert SecretHandshake.commands(31) === ["jump", "close your eyes", "double blink", "wink"]
-  end
-
-  test "do nothing for zero" do
-    assert SecretHandshake.commands(0) === []
-  end
-
-  test "do nothing if lower 5 bits not set" do
-    assert SecretHandshake.commands(32) === []
-  end
-
-  test "SecretHandshake.commands correctly returns no commands if lower 5 bits are not set" do
-    ptest number: int(min: 32) do
-      assert SecretHandshake.commands(number) === []
+  describe "SecretHandshake.commands/1" do
+    test "jump for 1000" do
+      assert SecretHandshake.commands(8) === ["jump"]
     end
-  end
-  
-  test "SecretHandshake.commands correctly returns commands" do
-    ptest number: int(min: 0, max: 31) do
-      actions = SecretHandshake.commands(number)
-      assert length(actions) >= 0
-      assert length(actions) <= 4
-      assert all?(actions, &(&1 in ["wink", "double blink", "close your eyes", "jump"]))
+
+    test "combine two actions" do
+      assert SecretHandshake.commands(3) === ["wink", "double blink"]
+    end
+
+    test "reverse two actions" do
+      assert SecretHandshake.commands(19) === ["double blink", "wink"]
+    end
+
+    test "reversing one action gives the same action" do
+      assert SecretHandshake.commands(24) === ["jump"]
+    end
+
+    test "reversing no actions still gives no actions" do
+      assert SecretHandshake.commands(16) === []
+    end
+
+    test "all possible actions" do
+      assert SecretHandshake.commands(15) === ["wink", "double blink", "close your eyes", "jump"]
+    end
+
+    test "reverse all possible actions" do
+      assert SecretHandshake.commands(31) === ["jump", "close your eyes", "double blink", "wink"]
+    end
+
+    test "do nothing for zero" do
+      assert SecretHandshake.commands(0) === []
+    end
+
+    test "do nothing if lower 5 bits not set" do
+      assert SecretHandshake.commands(32) === []
+    end
+
+    test "SecretHandshake.commands correctly returns no commands if lower 5 bits are not set" do
+      ptest number: int(min: 32) do
+        assert SecretHandshake.commands(number) === []
+      end
+    end
+    
+    test "SecretHandshake.commands correctly returns commands" do
+      ptest number: int(min: 0, max: 31) do
+        actions = SecretHandshake.commands(number)
+        assert length(actions) >= 0
+        assert length(actions) <= 4
+        assert all?(actions, &(&1 in ["wink", "double blink", "close your eyes", "jump"]))
+      end
     end
   end
 end
