cf8499b0863e5b866fabc266f4ce0f089f32a59b
Jeremy <jgmiller88@gmail.com>
2017-09-23 11:37:25 -0700

    finished strain exercise

--- lib/strain.ex
+++ lib/strain.ex
@@ -3,21 +3,39 @@ defmodule Strain do
   Solution for [Strain](http://exercism.io/exercises/elixir/strain/readme) exercise.
   """
 
+  import Enum, only: [reduce: 3]
+
   @doc ~S"""
   Given a `collection` and a `function`, returns a new collection containing those elements where the
   `function` is true.
-  """
-  @spec keep(list, fun()) :: list
-  def keep(collection, function) do
 
+  ## Examples
+
+      iex> require Integer
+      iex> Strain.keep([1, 2, 3], &Integer.is_odd/1)
+      [1, 3]
+      iex> Strain.keep([1, 2, 3, 4, 5], &Integer.is_even/1)
+      [2, 4]
+  """
+  @spec keep(list, (any() -> true | false)) :: list
+  def keep(collection, function) do
+    reduce(collection, [], fn(x, acc) -> if function.(x), do: acc ++ [x], else: acc end)
   end
 
   @doc ~S"""
   Given a `collection` and a `function`, returns a new collection containing those elements where the
   `function` is false.
-  """
-  @spec discard(list, fun()) :: list
-  def discard(collection, function) do
 
+  ## Examples
+
+      iex> require Integer
+      iex> Strain.discard([1, 2, 3], &Integer.is_odd/1)
+      [2]
+      iex> Strain.discard([1, 2, 3, 4, 5], &Integer.is_even/1)
+      [1, 3, 5]
+  """
+  @spec discard(list, (any() -> true | false)) :: list
+  def discard(collection, function) do
+    reduce(collection, [], fn(x, acc) -> if function.(x), do: acc, else: acc ++ [x] end)
   end
 end
--- test/strain_test.exs
+++ test/strain_test.exs
@@ -2,4 +2,63 @@ defmodule StrainTest do
   use ExUnit.Case, async: true
   use Quixir
   doctest Strain
+
+  defp noop(_), do: true
+
+  describe "Strain.keep/2" do
+    test "empty keep" do
+      assert Strain.keep([], &noop/1) == []
+    end
+
+    test "keep everything" do
+      assert Strain.keep([1, 2, 3], fn e -> e < 10 end) == [1, 2, 3]
+    end
+  
+    test "keep strings" do
+      words = ~w(apple zebra banana zombies cherimoya zelot)
+      assert Strain.keep(words, &(String.starts_with?(&1, "z"))) == ~w(zebra zombies zelot)
+    end
+  
+    test "keep arrays" do
+      rows = [
+        [1, 2, 3],
+        [5, 5, 5],
+        [5, 1, 2],
+        [2, 1, 2],
+        [1, 5, 2],
+        [2, 2, 1],
+        [1, 2, 5]
+      ]
+  
+      assert Strain.keep(rows, fn row -> 5 in row end) == [[5, 5, 5], [5, 1, 2], [1, 5, 2], [1, 2, 5]]
+    end
+  end
+
+  describe "Strain.discard/2" do
+    test "empty discard" do
+      assert Strain.discard([], &noop/1) == []
+    end
+
+    test "discard nothing" do
+      assert Strain.discard([1, 2, 3], fn e -> e > 10 end) == [1, 2, 3]
+    end
+  
+    test "discard strings" do
+      words = ~w(apple zebra banana zombies cherimoya zelot)
+      assert Strain.discard(words, &(String.starts_with?(&1, "z"))) == ~w(apple banana cherimoya)
+    end
+  
+    test "discard arrays" do
+      rows = [
+        [1, 2, 3],
+        [5, 5, 5],
+        [5, 1, 2],
+        [2, 1, 2],
+        [1, 5, 2],
+        [2, 2, 1],
+        [1, 2, 5]
+      ]
+      assert Strain.discard(rows, fn row -> 5 in row end) == [[1, 2, 3], [2, 1, 2], [2, 2, 1]]
+    end
+  end
 end
